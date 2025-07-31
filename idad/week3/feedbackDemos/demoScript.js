f826b5254793aa009f73c7a2733065b952284158
Patrick McMahon <patrick.mcmahon@rmit.edu.au>
2025-08-01 09:01:54 +1000

    added layout sections and load gifs

--- idad/week3/feedbackDemos/demoScript.js
+++ idad/week3/feedbackDemos/demoScript.js
@@ -1,13 +1,13 @@
 /* find custom colours */
 let bodyStyles = window.getComputedStyle(document.body);
-let col01 = bodyStyles.getPropertyValue('--col01');
-let col02 = bodyStyles.getPropertyValue('--col02');
+let col01 = bodyStyles.getPropertyValue("--col01");
+let col02 = bodyStyles.getPropertyValue("--col02");
 
 /* set up stage */
 let stage = new Konva.Stage({
-  container: 'stageContainer',
+  container: "stageContainer",
   width: window.innerWidth * 0.8,
-  height: window.innerWidth * 0.25
+  height: window.innerWidth * 0.25,
 });
 
 /////////////////////////////////////////////////////// Group 1 Drawing Canvas
@@ -25,47 +25,47 @@ let backgroundRectOne = new Konva.Rect({
   height: stage.height(),
   fill: col01,
   stroke: col02,
-  strokeWidth: 2
+  strokeWidth: 2,
 });
 
-const canvas = document.createElement('canvas');
+const canvas = document.createElement("canvas");
 canvas.width = backgroundRectOne.width();
 canvas.height = backgroundRectOne.height();
 
 const image = new Konva.Image({
   image: canvas,
   x: 0,
-  y: 0
+  y: 0,
 });
 
-const ctx = canvas.getContext('2d');
+const ctx = canvas.getContext("2d");
 ctx.strokeStyle = col02;
 ctx.lineJoin = "round";
 ctx.lineWidth = 1;
 let isPainting = false;
 let lastPointerPos;
-let mode = 'brush';
+let mode = "brush";
 
-function draw(){
-  if(mode === 'brush'){
-      ctx.globalCompositeOperation = 'source-over';
+function draw() {
+  if (mode === "brush") {
+    ctx.globalCompositeOperation = "source-over";
   }
 
-  if(mode === 'eraser'){
-      ctx.globalCompositeOperation = 'destination-out';
+  if (mode === "eraser") {
+    ctx.globalCompositeOperation = "destination-out";
   }
-  
+
   ctx.beginPath();
   const localPos = {
-      x: lastPointerPos.x - image.x(),
-      y: lastPointerPos.y - image.y()
+    x: lastPointerPos.x - image.x(),
+    y: lastPointerPos.y - image.y(),
   };
   ctx.moveTo(localPos.x, localPos.y);
 
   const pos = stage.getPointerPosition();
   const newLocalPos = {
-      x: pos.x - image.x(),
-      y: pos.y - image.y()
+    x: pos.x - image.x(),
+    y: pos.y - image.y(),
   };
   ctx.lineTo(newLocalPos.x, newLocalPos.y);
   ctx.closePath();
@@ -75,14 +75,14 @@ function draw(){
 
   layerOne.batchDraw();
 }
-image.on('mousedown', () => {
+image.on("mousedown", () => {
   isPainting = true;
   lastPointerPos = stage.getPointerPosition();
-  stage.on('mousemove', draw);
+  stage.on("mousemove", draw);
 });
 document.addEventListener("mouseup", () => {
   isPainting = false;
-  stage.off('mousemove', draw);
+  stage.off("mousemove", draw);
 });
 
 groupOne.add(backgroundRectOne);
@@ -90,11 +90,11 @@ groupOne.add(image);
 layerOne.add(groupOne);
 
 /* turn off cursor */
-groupOne.on('mouseover', (e) => {
-  e.target.getStage().container().style.cursor = 'none';
+groupOne.on("mouseover", (e) => {
+  e.target.getStage().container().style.cursor = "none";
 });
-groupOne.on('mouseout', (e) => {
-  e.target.getStage().container().style.cursor = 'default';
+groupOne.on("mouseout", (e) => {
+  e.target.getStage().container().style.cursor = "default";
 });
 
 /* 2 */
@@ -108,7 +108,7 @@ let backgroundRectTwo = new Konva.Rect({
   height: stage.height(),
   fill: col01,
   stroke: col02,
-  strokeWidth: 2
+  strokeWidth: 2,
 });
 
 let circle = new Konva.Circle({
@@ -118,7 +118,7 @@ let circle = new Konva.Circle({
   fill: col01,
   stroke: col02,
   strokeWidth: 4,
-  draggable: true
+  draggable: true,
 });
 
 document.getElementById("cacheButton").addEventListener("click", () => {
@@ -141,7 +141,7 @@ let backgroundRectThree = new Konva.Rect({
   height: stage.height(),
   fill: col01,
   stroke: col02,
-  strokeWidth: 2
+  strokeWidth: 2,
 });
 
 let crossCursor = new Konva.Star({
@@ -151,15 +151,15 @@ let crossCursor = new Konva.Star({
   innerRadius: 0,
   outerRadius: 12,
   stroke: col02,
-  strokeWidth: 2
+  strokeWidth: 2,
 });
 
 /* turn off cursor */
-groupThree.on('mouseover', (e) => {
-  e.target.getStage().container().style.cursor = 'none';
+groupThree.on("mouseover", (e) => {
+  e.target.getStage().container().style.cursor = "none";
 });
-groupThree.on('mouseout', (e) => {
-  e.target.getStage().container().style.cursor = 'default';
+groupThree.on("mouseout", (e) => {
+  e.target.getStage().container().style.cursor = "default";
 });
 
 let cursorDelay = 0;
@@ -171,7 +171,7 @@ document.getElementById("cursorDelayInput").addEventListener("change", (e) => {
 backgroundRectThree.on("mousemove", (e) => {
   e.evt.preventDefault();
   const pos = stage.getPointerPosition();
-  if(cursorDelay > 0){
+  if (cursorDelay > 0) {
     setTimeout(() => {
       crossCursor.position(pos);
     }, cursorDelay);
@@ -189,14 +189,7 @@ stage.add(layerTwo);
 let layerThree = new Konva.Layer();
 stage.add(layerThree); */
 
-
-
-
-
 /////////////////////////////////////////////////////// Button Demos
-function buttonTextChange(button){
+function buttonTextChange(button) {
   button.innerHTML = "you clicked me";
 }
-
-
-
